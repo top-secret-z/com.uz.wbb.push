705948433f0792d7c0a59690f32d56310c0fa4bf
Sascha Greuel <github@1-2.dev>
2022-09-18 22:46:47 +0200

    5.5.1

--- files/lib/data/thread/push/ThreadPushList.class.php
+++ files/lib/data/thread/push/ThreadPushList.class.php
@@ -1,17 +1,36 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wbb\data\thread\push;
+
 use wcf\data\DatabaseObjectList;
 
 /**
  * Represents a list of pushes.
- *  
- * @author		2020-2022 Zaydowicz
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.wbb.push
  */
-class ThreadPushList extends DatabaseObjectList {
-	/**
-	 * @inheritDoc
-	 */
-	public $className = ThreadPush::class;
+class ThreadPushList extends DatabaseObjectList
+{
+    /**
+     * @inheritDoc
+     */
+    public $className = ThreadPush::class;
 }
